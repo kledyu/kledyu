0d6ca8da27c70b835c84d9045e0f38edd3273c83
Kled <1017yu@daum.net>
2025-02-27 17:46:49 +0900

    fix: 다크모드 변경 토글 최초 렌더 시 `system`인 경우 수정

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -3,12 +3,17 @@
 import { cn } from '@/lib/utils';
 import { motion } from 'framer-motion';
 import { Moon, Sun } from 'lucide-react';
-import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export default function ThemeToggle() {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  const isDarkTheme =
+    theme === 'dark' ||
+    (theme === 'system' &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches);
 
   useEffect(() => {
     setMounted(true);
@@ -20,17 +25,17 @@ export default function ThemeToggle() {
 
   return (
     <button
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={() => setTheme(isDarkTheme ? 'light' : 'dark')}
       className={cn(
         'relative h-8 w-16 rounded-full p-1 ring-1 transition-all duration-300 ml-auto',
-        theme === 'dark'
+        isDarkTheme
           ? 'bg-slate-200 ring-slate-100 hover:bg-slate-300'
           : 'bg-slate-900 ring-slate-200 hover:bg-slate-700'
       )}>
       <motion.div
         animate={{
-          x: theme === 'dark' ? '2rem' : '0rem',
-          rotate: theme === 'dark' ? 230 : 0,
+          x: isDarkTheme ? '2rem' : '0rem',
+          rotate: isDarkTheme ? 230 : 0,
         }}
         transition={{
           type: 'spring',
@@ -40,11 +45,11 @@ export default function ThemeToggle() {
         }}
         className={cn(
           'flex h-6 w-6 items-center justify-center rounded-full',
-          theme === 'dark'
+          isDarkTheme
             ? 'bg-slate-950 text-slate-200'
             : 'bg-white text-yellow-500 shadow-md'
         )}>
-        {theme === 'dark' ? (
+        {isDarkTheme ? (
           <Moon className='h-4 w-4' />
         ) : (
           <Sun className='h-4 w-4' />
